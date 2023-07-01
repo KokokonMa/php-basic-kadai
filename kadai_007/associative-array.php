d8a25d5d208de58399180617bb071f7019eabf14
KokokonMa <137073671+KokokonMa@users.noreply.github.com>
2023-07-01 09:57:13 +0900

    Update associative-array.php

--- kadai_007/associative-array.php
+++ kadai_007/associative-array.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
 
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <P>
+    <?php
+      $item=['name'=>'onion','price'=>200,'weight'=>160];
+      print_r($item);
+    ?>
+
+  </P>
+</body>
+
+</html>
