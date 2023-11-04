10b4a2cdc431931d3f389845dd9ba9ea3b57acd1
KokokonMa <137073671+KokokonMa@users.noreply.github.com>
2023-11-04 18:10:37 +0900

    Update class.php

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -1,1 +1,50 @@
+<?php
 
+class Food{
+  private $name;
+  private $price;
+
+  //コンストラクタを作成する
+  public function __construct(string $name, int $price){
+    $this->name= $name;
+    $this->price=$price;
+  }
+
+
+  //メソッドを定義する
+  //「price」プロパティの値を出力する「show_price」メソッド
+  public function show_price(){
+    echo "{$this->name}の価格は{$this->price}円です。<br>";
+  }
+
+}
+
+class Animal{
+  private $name;
+  private $height;
+  private $weight;
+
+  //コンストラクタを作成する
+  public function __construct(string $name, int $height, float $weight){
+    $this->name= $name;
+    $this->height=$height;
+    $this->weight=$weight;
+  }
+
+  //メソッドを定義する
+  //「height」 プロパティの値を出力する「show_height」メソッド
+  public function show_height(){
+    echo "{$this->name}の高さは{$this->height}cmです。<br>";
+  }
+
+}
+
+//Foodをインスタンス化する
+$potato=new Food('ジャガイモ', 300);
+$potato->show_price();
+
+//Animalをインスタンス化する
+$cat=new Animal('猫', 20, 3.5);
+$cat->show_height();
+
+?>
